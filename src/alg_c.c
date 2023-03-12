50e620bd29f74d820baab70f06a742a4262815e1
Paul  Houssel <paul.houssel@telecom-sudparis.eu>
2023-03-12 16:34:08 +0100

    handle debugging

--- src/alg_c.c
+++ src/alg_c.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BLOCK_SIZE 1024
+//#define DEBUG 0
+#ifdef DEBUG
+#define DEBUG_PRINT(fmt, args...)    fprintf(stderr, fmt, ## args)
+#else
+#define DEBUG_PRINT(fmt, args...)    /* Don't do anything in release builds */
+#endif
 
 // Function to compare two strings lexicographically
 int string_compare(const void *a, const void *b) {
@@ -12,7 +17,7 @@ int string_compare(const void *a, const void *b) {
 }
 
 // Function to rotate a string by one character
-void rotate_string(char *str, int len) {
+void shift_cyclic(char *str, int len) {
     char first = str[0];
     for (int i = 1; i < len; i++) {
         str[i - 1] = str[i];
@@ -20,20 +25,31 @@ void rotate_string(char *str, int len) {
     str[len - 1] = first;
 }
 
-// Function to compute the output of the algorithm
+// Algorithm C: Compression Algorithm
 void alg_c(char *S, int N, char **L, int *I) {
-    // Allocate memory for the matrix M
+
+    // Allocate memory for the matrix M of size N by N
     char **M = (char **)malloc(N * sizeof(char *));
+    
+    // Allocate memory for each row of the matrix
     for (int i = 0; i < N; i++) {
         M[i] = (char *)malloc((N + 1) * sizeof(char));
     }
 
     // Generate the matrix M by cyclically shifting S
-    // and sorting the rows lexicographically
     for (int i = 0; i < N; i++) {
         strcpy(M[i], S);
-        rotate_string(S, N);
+        shift_cyclic(S, N);
     }
+    for (int row=0; row<N; row++)
+    {
+        for(int columns=0; columns<N; columns++)
+        {
+            DEBUG_PRINT("%c     ", M[row][columns]);
+        }
+        printf("\n");
+    }
+
     qsort(M, N, sizeof(char *), string_compare);
 
     // Find the index I of the row containing the original string S
