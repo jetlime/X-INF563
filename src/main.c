28b1acef419f5c2e08c48fb100765b9c80308b7f
Paul  Houssel <paul.houssel@telecom-sudparis.eu>
2023-03-15 09:59:01 +0100

    read from file

--- src/main.c
+++ src/main.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "debug.h"
 
 void alg_c(char *S, int N, char *L, int *I);
 char* alg_d(char *L, int *I);
 
 int main() {
-    // read input file
-    /*
-    FILE* input_file = fopen("encode.txt", "r");
-    if (!input_file)
-        exit(EXIT_FAILURE);
-
+    // read from file
     struct stat sb;
     if (stat("encode.txt", &sb) == -1) {
         perror("stat");
         exit(EXIT_FAILURE);
     }
 
-    char* S = malloc(sb.st_size);
-    fread(S, sb.st_size, 1, input_file);
+    FILE *input_file = fopen("encode.txt", "rb");
+    if (input_file == NULL) {
+        printf("Error: Failed to open the file.\n");
+        return 1;
+    }
 
-    printf("%s\n", S);
-
+    // Read the text from the file into S
+    char S[sb.st_size];
+    if (fread(S, sb.st_size, 1,input_file)< 1) {
+        printf("Error: Failed to read the file.\n");
+        fclose(input_file);
+        return 1;
+    }
+    S[sb.st_size] = '\0';
+    
+    // Close the file
     fclose(input_file);
-    free(S);
-    */
-
-    char S[] = "information theory";
+    
+    //char S[] = "information theory";
     int N = strlen(S);
     char *L = (char *)malloc(N * sizeof(char));
     int I;
